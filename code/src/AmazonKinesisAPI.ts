8e3df046dc2761e0a65d67bc4d0eb9486cb2453a
rug <rmfgoncalves@gmail.com>
2021-05-23 15:23:07 +0300

    Stable point

--- code/src/AmazonKinesisAPI.ts
+++ code/src/AmazonKinesisAPI.ts
@@ -1,13 +1,16 @@
-const AWS = require('@aws-sdk/client-kinesis-video');
+//const AWS = require('@aws-sdk/client-kinesis-video');
+//// <reference path="../node_modules/@aws-sdk/client-kinesis-video/dist/types/KinesisVideoClient/KinesisVideoClient.d.ts" />
+/// <reference types="aws-sdk" />
+/// referece "../node_modules/@aws-sdk/client-kinesis-video/dist/types/KinesisVideoClient";
 
-export namespace AmazonKinesisAPI {
-    export function Init() {
-        const client = new AWS.KinesisVideoClient({ region: "REGION" });
-        
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+namespace AmazonKinesisAPI {
+    export function Init(): AWS.KinesisVideoMedia {
+        //const client = new AWS.KinesisVideoClient({ region: 'REGION' });
+        return new AWS.KinesisVideoMedia({});
     }
 
-    export function startCall() {
-        
+    export function StartCall(): unknown {
+        return undefined;
     }
-
 }
